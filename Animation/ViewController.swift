d531f621afee520f54e1f332ddbb1e383b4e7380
sthordall <stephantl@gmail.com>
2015-08-15 14:04:02 +0200

    Initial commit with empty project

---
+++ Animation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by Stephan Thordal Larsen on 15/08/15.
+//  Copyright (c) 2015 Stephan Thordal Larsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
